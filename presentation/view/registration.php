28fef2c1d42d5bc1f53747b485f5fce4ea6de0b0
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-11-29 10:17:21 -0800

    Added code to registration page. Fixed CSS on registration page.

--- presentation/view/registration.php
+++ presentation/view/registration.php
@@ -1,22 +1,42 @@
+<!DOCTYPE html>
+<html>
 
-<form>
-<h2>Register New Account</h2>
-  <div class="form-group">
-    <label for="email"></label>
-    <input type="text" class="form-control" id="email" placeholder="Email Address">
-  </div>
-  <div class="form-group">
-    <label for="username"></label>
-    <input type="text" class="form-control" id="username" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <label for="password"></label>
-    <input type="text" class="form-control" id="password" placeholder="password">
-  </div>
-  <div class="form-group">
-    <label for="Re-enter Password"></label>
-    <input type="text" class="form-control" id="password2" placeholder="Re-enter Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-<?php
+	<head>
+		<link rel="stylesheet" type="text/css" href="/presentation/css/style.css">
+		<title>Registration</title>
+	</head>
+	
+	<body>
+		<div class="form-signin">
+            <form action="/presentation/handlers/registrationHandler.php" method="POST">
+            	<h2>Register New Account</h2>
+
+              <div class="form-group">
+                <label for="email"></label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="Email Address">
+              </div>
+              
+              <div class="form-group">
+                <label for="username"></label>
+                <input type="text" name="userName" id="username" placeholder="Username">
+              </div>
+              
+              <div class="form-group">
+                <label for="password"></label>
+                <input type="password" name="password1" id="password" placeholder="password">
+              </div>
+              
+              <div class="form-group">
+                <label for="Re-enter Password"></label>
+                <input type="password" name="passsword2" id="password2" placeholder="Re-enter Password">
+              </div>
+			  
+			  <div class="form-group">
+                <button type="submit" class="btn btn-primary">Register</button>
+              </div>
+              
+            </form>
+        </div>
+    </body>
+    
+</html>
