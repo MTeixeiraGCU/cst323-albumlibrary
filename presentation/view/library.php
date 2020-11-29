243d7c2a146aede2e34494f2c683fc6abfe1b33a
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-11-29 14:14:02 -0800

    Added a noimage image and fixed the cards in the library.

--- presentation/view/library.php
+++ presentation/view/library.php
@@ -7,11 +7,35 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/presentation/handlers/libraryHandler.
 <h1>Your Library</h1>
 <!-- This section is going to need a handler that will return all database entries for albums and fill a card out for each.  -->
 
-<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-  </div>
+<?php 
+
+foreach($albums as $album) {
+?>
+<div class="row" style="width: 100%; margin: auto;">
+
+<?php 
+    for($i = 0; $i <= 2; $i++) {
+
+        echo "<div class='card' style='width: 18rem; margin: auto;'>";
+        echo "  <img src='/presentation/media/" . $album['IMG_LINK'] . "' class='card-img-top' alt='...'>";
+        echo "  <div class='card-body'>";
+        echo "      <h5 class='card-title'>" . $album['ALBUM_TITLE'] . "</h5>";
+        echo "      <p class='card-text'>Release Date: " . $album['POST_TIME'] . "</p>";
+        echo "      <p class='card-text'>Artist: " . $album['ARTIST'] . "</p>";
+        echo "      <p class='card-text'>Rating: " . $album['RATING'] . "/5</p>";
+        echo "      <p class='card-text'>Description: " . $album['DESCRIPTION'] . "</p>";
+        echo "      <!-- <a href='#' class='btn btn-primary'>Go somewhere</a> -->";
+        echo "  </div>";
+        echo "</div>";
+        
+        $album = next($albums);
+        if(is_null($album)){
+            break;
+        }
+    }
+?>
 </div>
+
+<?php 
+}
+?>
