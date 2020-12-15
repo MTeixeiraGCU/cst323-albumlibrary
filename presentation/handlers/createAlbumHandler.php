40f438355100aab9340dfbb2334e419ba3e11a21
Marc Teixeira <MTeixeira@my.gcu.edu>
2020-12-14 17:42:24 -0800

    Added initial files for album creation.

---
+++ presentation/handlers/createAlbumHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/autoloader.php';
+
+//check each of the required fields and populate thier error message as necessary.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+}
+
+?>
---
+++ presentation/view/albumAdded.php
@@ -0,0 +1,23 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/autoloader.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="/presentation/css/style.css">
+		<title>Album Added</title>
+	</head>
+	
+	<body>
+		<div class="page-heading">
+			<h1>The album has been added to your library!</h1>
+			<a href="/presentation/view/library.php">Go to Library</a>
+		</div>
+		
+	</body>
+	
+	
+</html>
